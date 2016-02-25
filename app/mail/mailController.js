4b8c63b43807aa9d588720697c6c894078eae65e
vtsaruk <vitaliy.tsaruk@gmail.com>
2016-02-25 15:05:55 +0200

    messages

--- app/mail/mailController.js
+++ app/mail/mailController.js
@@ -1,18 +1,30 @@
 module.exports = mailController;;
 
-function mailController (mailService, $cookies) {
-  $cookies.put('PHPSESSID', 'jar9vlgoddf0puj6fl6scuifh6');
-  // this.getMessages = function () {
-  //   this.messages = mailService.getAllMessages()
-  // };
 
- console.log(mailService.getAllMessages().$promise.then(
+function mailController (mailService, $cookies) {
+
+  $cookies.put('PHPSESSID', 'jar9vlgoddf0puj6fl6scuifh6');
+  this.getMessages = function () {
+     mailService.getAllMessages().$promise.then(
       function(data) {
-        console.log(data);
+        this.messages = data;
       }, function(error) {
         console.log(error);
       }
-    ));;
+  )};
+     this.getMessages();
+console.log(this.messages);
+
+
+ // this.messges = mailService.getAllMessages().$promise.then(
+ //      function(data) {
+ //        this.messges = data;
+ //        console.log('hello')
+ //      }, function(error) {
+ //        console.log(error);
+ //      }
+ //    );
+ // console.log(this.messges);
 }
 
 mailController.$inject = ['mailService', '$cookies'];
